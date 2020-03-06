59c75d5e381502c013cc7c62f59a280984973261
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-03-06 19:01:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,21 +1,3 @@
-# code your #position_taken? method here!
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def move(board, index, user_marker="X")
-  board[index.to_i] = user_marker
-end
-
-def input_to_index(users_input)
-  users_input.to_i - 1
-end
-
 def position_taken?(board, index)
-  if index == " "
-    "false"
+
 end
